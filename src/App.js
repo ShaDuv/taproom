95ccedef2a0624e40417fa7e2440a849aaaeb131
Shawna Duvall <shawnaduvall@gmail.com@>
2019-09-06 10:18:21 -0700

    add google font. Discovered you can install all of them through NPM

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TapList from './Taps/TapList';
-
+import Banner from "./Header/Banner"
 function App() {
   return (
     <div className='App'>
+      <Banner/>
       <TapList/>
     </div>
   );
---
+++ src/Header/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import css from './banner.css';
+
+function Banner() {
+  return (
+    <div className='banner'>
+      <h1>Tapped Out</h1>
+    </div>
+  );
+}
+export default Banner;
