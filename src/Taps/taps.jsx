0b87d19db76e4417853faee0a004a6fa52482738
Shawna Duvall <shawnaduvall@gmail.com@>
2019-09-06 16:31:59 -0700

    fix the error I was getting from the ID being a number, made it a string to make it stop but figured otu it can't start with a 0 and then can be a number.

--- src/Taps/taps.jsx
+++ src/Taps/taps.jsx
@@ -1,92 +1,101 @@
 const taps = [
   {
-    id: '01',
+    id: 1,
     name: 'Joe IPA',
     description: 'Aroma is hoppy and sweet, citrus and pine. Flavor is citrus sweetness and moderate hoppy bitters. Lightly malty. Good balance and refreshing.',
     brewery: '10 Barrel',
     style: 'India Pale Ale (IPA)',
     abv: '6.9%',
-    price: '$5.25'
+    price: '$5.25',
+    pints: 124
   },
 
   {
-    id: '02',
+    id: 2,
     name: 'Bad Apple',
     description: 'The Bad Apple is a semi-dry, bold cider. Aged with brandy-cured Oregon white oak, the Bad Apple offers complex oak tannins and rich vanilla notes. Hints of sweetness derived from local meadowfoam honey make this a cider to savor.',
     brewery: '2 Towns',
     style: 'Cider',
     abv: '10.5%',
-    price: '$6.50'
+    price: '$6.50',
+    pints: 124
   },
 
   {
-    id: '03',
+    id: 03,
     name: 'Made Marion',
     description: 'The sunrise of summer, the Oregon Made Marion is a blend of Oregon grown Marionberries and hard apple cider. Ripe berry flavors and a crisp apple bite combine with a rich ruby color to make this cider a delicious way to celebrate the bounty of Oregon!',
     brewery: '2 Towns',
     style: 'Cider',
     abv: '6%',
-    price: '$6.00'
+    price: '$6.00',
+    pints: 124
   },
 
   {
-    id: '04',
+    id: 4,
     name: 'Pacific Pineapple',
     description: 'Unfiltered Pineapple Cider. Juicy and tropical Pacific Pineapple rolls ripe Costa Rica golden pineapples into fresh-pressed Northwest apples. This refreshingly juicy and easy drinking session cider will relax your state of mind no matter your locale!',
     brewery: '2 Towns',
     style: 'Cider',
     abv: '5%',
-    price: '$6.00'
+    price: '$6.00',
+    pints: 124
   },
 
   {
-    id: '05',
+    id: 05,
     name: 'Amber',
     description: 'The name of this beer style comes from the German word "alt" meaning "old". This refers to the aging that alts undergo since they ferment more slowly and at colder temperatures than most ales. Slow fermentation helps condition the flavors in Alaskan Amber, contributing to its overall balance and smoothness.',
     brewery: 'Alaskan',
     style: 'Altbier',
     abv: '5.3%',
-    price: '$5.00'
+    price: '$5.00',
+    pints: 124
   },
 
   {
-    id: '06',
+    id: 6,
     name: 'Budweiser',
     description: "Brewed and sold since 1876: 'The King of Beers' is the largest-selling beer in the world. Budweiser has been the world's best-selling beer since 1957, and is distributed in more than 70 countries. Budweiser leads the U.S. Premium beer category, outselling all other domestic premium beers combined. In fact, one in almost every five beers sold in the United States today is a Budweiser.",
     brewery: 'Anhuaser Busch',
     style: 'Pale Lager',
     abv: '6.9%',
-    price: '$5.25'
+    price: '$5.25',
+    pints: 124
   },
 
   {
-    id: '07',
+    id: 7,
     name: 'Carina Peach',
     description: 'The name is from the constellation, Carina, the keel of the ship Argo in the southern sky. Loaded with fresh peach flavor it is a sour ale perfect for anytime of year. The use of pale malt makes for a crisp character, while lactobacillus gives it a tart and refreshing finish.',
     brewery: 'Ecliptic',
     style: 'Sour/Wild Ale',
     abv: '5.5%',
-    price: '$5.50'
+    price: '$5.50',
+    pints: 124
   },
 
   {
-    id: '08',
+    id: 8,
     name: 'Cavatica Stout',
     description: 'An immense, bold, black as night, american stout beauty; Cavatica is no dry Irish anything. Made in the tradition of terminator, except stronger, one sip and you will surely take notice of what youre drinking. The name honors this brewers affinity with arachnia. Be forewarned, this beer will stain your clothes.',
     brewery: 'Fort George',
     style: 'Stout',
     abv: '8.5%%',
-    price: '$6.50'
+    price: '$6.50',
+    pints: 124
   },
 
   {
-    id: '09',
+    id: 9,
     name: 'Total ReKolsch',
     description: 'An awakening of selective memory loss, this is a tip of the cap to our friends who previously occupied this space. Our brewmaster Robs first commercial brew years ago was a Kolsch and he has rewritten this recipe for year round delight. Pilsner nose, Noble hops on the palate, ale fermented at cooler temps and lagered for a clean crisp finish with a touch of sweetness.',
     brewery: 'Stormbreaker',
     style: 'Kolsch',
     abv: '5.2%',
-    price: '$5.50'
+    price: '$5.50',
+    pints: 124
   }
 
 ];
