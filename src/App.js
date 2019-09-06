d96febb01980053ee8dd466cea8c9ea9c0fbc9f2
Shawna Duvall <shawnaduvall@gmail.com@>
2019-09-06 11:13:02 -0700

    add text to top but it creates a gap I can't resolve

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TapList from './Taps/TapList';
-import Banner from "./Header/Banner"
+import Banner from './Header/Banner';
 function App() {
   return (
     <div className='App'>
